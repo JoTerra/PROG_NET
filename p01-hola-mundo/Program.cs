10c686ae5b1a392dd96c27c2f3a11f882bbe0f15
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-02-13 20:40:14 -0600

    Add files via upload

---
+++ p01-hola-mundo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string amigo = "Juan";
+string hermano = "Pablo";
+
+Console.WriteLine(amigo);
+amigo = "Miguel";
+Console.WriteLine(amigo);
+Console.WriteLine($"Hola {amigo} bienvenido a C#");
+Console.WriteLine($"\nMi amigo es {amigo}, mi hermano es {hermano}\n");
+Console.WriteLine("Nombres con mayusculas");
+Console.WriteLine($"{amigo.ToUpper()} {hermano.ToUpper()}");
