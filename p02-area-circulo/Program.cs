92eb23e9841b22b4f139be9b7f474c4eae20ac09
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-02-13 20:40:41 -0600

    Add files via upload

---
+++ p02-area-circulo/Program.cs
@@ -0,0 +1,8 @@
+﻿double area, radio;
+Console.WriteLine("Calculando el area de un circulo:\n");
+Console.Write("Dame el radio > ");
+radio = double.Parse(Console.ReadLine());
+area = Math.PI * Math.Pow(radio,2);
+Console.WriteLine($"El circulo de radio {radio} tiene un area de {area}");
+Console.WriteLine(radio.GetType());
+Console.WriteLine(area.GetType());
