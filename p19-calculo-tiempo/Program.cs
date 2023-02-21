d53c5285ed11111ccddf36dee0a0281bb69bef9e
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-02-21 08:14:47 -0600

    Add files via upload

---
+++ p19-calculo-tiempo/Program.cs
@@ -0,0 +1,16 @@
+﻿int dias,hr,min,sec;
+
+Console.WriteLine("Inserte Horas a Convertir");
+hr = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+dias = hr / 24;
+hr = hr % 24;
+min = hr * 60;
+sec = min * 60;
+
+Console.WriteLine("Dias: "+dias);
+Console.WriteLine("Minutos: "+min);
+Console.WriteLine("Segundos: "+sec);
+
+
