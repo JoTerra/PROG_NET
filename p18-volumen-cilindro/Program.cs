9f13bde7ec24a163d51dde4f8b3037cfe91fd267
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-02-21 08:14:04 -0600

    Add files via upload

---
+++ p18-volumen-cilindro/Program.cs
@@ -0,0 +1,9 @@
+﻿double vol, rad, alt;
+const double pi = 3.1415926;
+Console.WriteLine("Escriba el radio deseado");
+rad = double.Parse(Console.ReadLine());
+Console.WriteLine("Escriba la altura deseada");
+alt = double.Parse(Console.ReadLine());
+
+vol = pi * Math.Pow(rad,2) * alt;
+Console.WriteLine("El volumen de este cilindro es: "+ vol);
