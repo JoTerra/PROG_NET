38f5206bb6b104337186306aafdf2af8b6640263
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-02-21 08:13:32 -0600

    Add files via upload

---
+++ p17-convertir-temperatura/Program.cs
@@ -0,0 +1,6 @@
+﻿double temp1, temp2;
+Console.WriteLine("Ingrese la temperatura(°F) a convertir");
+temp1 = double.Parse(Console.ReadLine());
+
+temp2 = (temp1 - 32) * (5/9);
+Console.WriteLine("La Temperatura es: "+temp2);
