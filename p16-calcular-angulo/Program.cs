1e967f40f1e97a6798162c6d63e672a8394712fb
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-02-21 08:12:56 -0600

    Add files via upload

---
+++ p16-calcular-angulo/Program.cs
@@ -0,0 +1,7 @@
+﻿double ang1, ang2, ang3;
+Console.WriteLine("Inserte Angulo 1");
+ang1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Inserte Angulo 2");
+ang2 = double.Parse(Console.ReadLine());
+ang3 = 180 - (ang1 + ang2);
+Console.WriteLine("El Resultado(Angulo 3) es: " + ang3);
