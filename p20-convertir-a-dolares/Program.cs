960b9ed477e692b37b3a0a94b31d9c498726cc7a
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-02-21 08:15:22 -0600

    Add files via upload

---
+++ p20-convertir-a-dolares/Program.cs
@@ -0,0 +1,8 @@
+﻿double mxn, dlr;
+double cot = 18.3970;
+
+Console.WriteLine("Ingrese cantidad (MXN) a convertir");
+mxn = double.Parse(Console.ReadLine());
+
+dlr = mxn * cot;
+Console.WriteLine("Dolares: "+dlr);
