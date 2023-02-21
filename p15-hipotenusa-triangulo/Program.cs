2c56f0ae2549ade8c19440dbe00f72d57bddb3bb
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-02-21 08:12:23 -0600

    Add files via upload

---
+++ p15-hipotenusa-triangulo/Program.cs
@@ -0,0 +1,8 @@
+﻿double l1,l2,hip;
+Console.WriteLine("Ingrese el Lado 1");
+l1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el Lado 2");
+l2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la Hipotenusa");
+hip = Math.Sqrt(Math.Pow(l1,2) + Math.Pow(l2,2));
+Console.WriteLine("La hipotenusa resulta en : " +hip);
