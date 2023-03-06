75e8be341a59a23eb6ac35b716ea2b2f0fd0270d
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-03-05 20:18:11 -0600

    Add files via upload

---
+++ p35-continentes-mundo/Program.cs
@@ -0,0 +1,12 @@
+﻿int op;
+Console.WriteLine("Bienvenido, Si quieres conocer los continentes, escribe un numero (1-6)!");
+op = int.Parse(Console.ReadLine());
+    switch(op){
+        case 1: Console.WriteLine("Asia"); break;
+        case 2: Console.WriteLine("África"); break;
+        case 3: Console.WriteLine("América del Norte"); break;
+        case 4: Console.WriteLine("América del Sur"); break;
+        case 5: Console.WriteLine("Antártida"); break;
+        case 6: Console.WriteLine("Europa"); break;
+        default: Console.WriteLine("Ooops... Numero Invalido!"); break;
+    }
