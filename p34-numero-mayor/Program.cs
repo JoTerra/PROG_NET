0f1c743d670f041a1106bf2c7aadf248fdd1ab27
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-03-05 20:16:38 -0600

    Add files via upload

---
+++ p34-numero-mayor/Program.cs
@@ -0,0 +1,15 @@
+﻿int num1,num2,num3;
+
+Console.Clear();
+Console.WriteLine("Introduzca numeros");
+num1 = int.Parse(Console.ReadLine());
+num2 = int.Parse(Console.ReadLine());
+num3 = int.Parse(Console.ReadLine());
+
+if(num1 > num2 && num1 > num3){
+    Console.WriteLine($"El numero 1: {num1} - es el mayor!");
+}else if(num2 > num1 && num2 > num3){
+    Console.WriteLine($"El numero 2: {num2} - es el mayor!");
+}else{
+    Console.WriteLine($"El numero 3: {num3} - es el mayor!");
+}
