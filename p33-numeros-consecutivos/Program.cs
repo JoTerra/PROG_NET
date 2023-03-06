97b36a422bd070d2d2b5e5c6fd377118fd63618d
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-03-05 20:15:38 -0600

    Add files via upload

---
+++ p33-numeros-consecutivos/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1,num2,num3;
+
+Console.Clear();
+Console.WriteLine("Introduzca numeros");
+num1 = int.Parse(Console.ReadLine());
+num2 = int.Parse(Console.ReadLine());
+num3 = int.Parse(Console.ReadLine());
+
+if(num1 + 1 == num2 && num2 + 1 == num3){
+    Console.WriteLine("Los Numeros son consecutivos!");
+}else{
+    Console.WriteLine("Lo siento, los numeros no son consecutivos");
+}
