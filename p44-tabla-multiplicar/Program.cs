d7069687889339623aa8ba6835ed6043a82a1031
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-03-09 11:24:06 -0600

    Add files via upload

---
+++ p44-tabla-multiplicar/Program.cs
@@ -0,0 +1,13 @@
+﻿int c,t,n;
+Console.Clear();
+Console.WriteLine("Imprimir la tabla deseada de 1 a 10\n");
+Console.Write("Que tabla quieres ? ");
+t = int.Parse(Console.ReadLine());
+Console.Write("Hasta donde ? ");
+n = int.Parse(Console.ReadLine());
+c = 1;
+while( c <= n ) {
+    Console.WriteLine($"{c} x {t} = {c*t}");
+    c++;
+}
+Console.WriteLine("\nProceso terminado ...");
