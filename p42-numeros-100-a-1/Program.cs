a6b6f85c8f405fcc99da2eda3597d6da581ca48f
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-03-09 11:22:59 -0600

    Add files via upload

---
+++ p42-numeros-100-a-1/Program.cs
@@ -0,0 +1,32 @@
+﻿//Descomente los ejemplos
+
+//41a
+int c;
+c = 100;
+while( c >= 1 ) {
+    Console.Write($"{c} ");
+    c--;
+}
+
+
+//41b
+/*
+Console.WriteLine("Escriba hasta que numero desea llegar");
+int c = int.Parse(Console.ReadLine());
+int a = 100;
+while (a >= c ){
+    Console.Write($"{a} ");
+    a--;
+}
+*/
+
+//41c
+/*
+Console.WriteLine("¿De cuanto en cuanto deseas saltar?");
+int counter = int.Parse(Console.ReadLine());
+int c = 100;
+while(c >= 1){
+    Console.Write($"{c} ");
+    c -= counter;
+}
+*/
