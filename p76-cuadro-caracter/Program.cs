0955e06f0e4ac0d87cd9e35f2d22c788079ace92
JoTerra <54294285+JoTerra@users.noreply.github.com>
2023-04-01 10:31:23 -0600

    Add files via upload

---
+++ p76-cuadro-caracter/Program.cs
@@ -0,0 +1,15 @@
+﻿void cuadro(int r, int c, char car) {
+    for(int i=1;i<=r;i++) {
+        for(int j=1;j<=c;j++)
+        Console.Write($"{car} ");
+        Console.WriteLine("");
+    }
+}
+int r,c;
+char car;
+Console.Clear();
+Console.Write("Renglones : "); r = int.Parse(Console.ReadLine());
+Console.Write("Columnas : "); c = int.Parse(Console.ReadLine());
+Console.WriteLine("De que caracter ? ");
+car = Console.ReadLine()[0];
+cuadro(r,c,car);
